f5ead406ba61d8add30f714a16c282e5c1abf3af
cosxt <cosxtgx@gmail.com>
2015-11-23 22:17:30 +0200

    2311

---
+++ src/main/java/com/audev/common/Config/WebAppConfig.java
@@ -0,0 +1,34 @@
+package com.audev.common.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by cosxt on 28.10.2015.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.audev.common")
+public class WebAppConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupVR() {
+        InternalResourceViewResolver ir = new InternalResourceViewResolver();
+
+        ir.setPrefix("/pages/");
+        ir.setSuffix(".jsp");
+        ir.setViewClass(JstlView.class);
+
+        return ir;
+    }
+}
